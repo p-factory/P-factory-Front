aadfecaf4b4f6d7287c32057165c184c3c1da0e2
4Bee <4bee.code@gmail.com>
2024-12-22 21:41:27 +0900

    🧪: 전체 좌우 여백 수정, 테스트 및 확인

--- front/src/View/TestPage.tsx
+++ front/src/View/TestPage.tsx
@@ -2,7 +2,14 @@ import styles from './TestPage.module.scss';
 
 const TestPage = () => {
   return (
-    <div className={styles.background}>
+    <div
+      className={styles.debug}
+      style={{
+        width: '100%',
+        // maxWidth: '800px',
+        // minWidth: '400px',
+      }}
+    >
       <span>test</span>
     </div>
   );
