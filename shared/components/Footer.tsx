f5dd7f49dc267e11af5781360cbe4254609e55eb
4Bee <4bee.code@gmail.com>
2025-01-01 00:20:54 +0900

    ✨: Footer HTML 요소 작성 및 공유 가능한 컴포넌트로 코드 작성, css 미적용

---
+++ shared/components/Footer.tsx
@@ -0,0 +1,31 @@
+import { Platform, Text } from 'react-native';
+import { StylesProps } from '../type';
+
+const Footer = ({ styles }: { styles: StylesProps }) => {
+  if (Platform.OS === 'web') {
+    return (
+      <div id={styles.footer}>
+        <div className={styles.contents}>
+          <div className={styles.image}>image</div>
+          <span className={styles.title}>내부공장</span>
+        </div>
+        <div className={styles.contents}>
+          <div className={styles.image}>image</div>
+          <span className={styles.title}>외부공장</span>
+        </div>
+        <div className={styles.contents}>
+          <div className={styles.image}>image</div>
+          <span className={styles.title}>대탈출</span>
+        </div>
+        <div className={styles.contents}>
+          <div className={styles.image}>image</div>
+          <span className={styles.title}>마이페이지</span>
+        </div>
+      </div>
+    );
+  }
+
+  return <Text style={{ color: '#fff' }}>This is None!</Text>;
+};
+
+export default Footer;
