1f934c9a321eaeedc0a850e14bfe6c558c8d3955
4Bee <4bee.code@gmail.com>
2025-01-01 16:00:47 +0900

    🔧: Footer에 들어간 이미지 assets로 설정

--- front/src/assets/assets.ts
+++ front/src/assets/assets.ts
@@ -1,9 +1,17 @@
 import reactLogo from './img/react.svg';
 import viteLogo from '/vite.svg'; // public 폴더 파일은 루트 경로로 접근 가능
+import escapeGameIcon from './img/escape-game-icon.svg';
+import innerFactoryIcon from './img/inner-factory-icon.svg';
+import outerFactoryIcon from './img/outer-factory-icon.svg';
+import mypageIcon from './img/mypage-icon.svg';
 
-const assets = {
+const Assets = {
   reactLogo,
   viteLogo,
+  escapeGameIcon,
+  innerFactoryIcon,
+  outerFactoryIcon,
+  mypageIcon,
 };
 
-export default assets;
+export default Assets;
