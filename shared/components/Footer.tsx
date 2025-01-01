184b25fecadf0f47b501610488453a419b15a7eb
4Bee <4bee.code@gmail.com>
2025-01-01 16:03:44 +0900

    💄 : footer css 적용 완료

--- shared/components/Footer.tsx
+++ shared/components/Footer.tsx
@@ -1,5 +1,6 @@
 import { Platform, Text } from 'react-native';
 import { StylesProps } from '../type';
+import Assets from '../../front/src/assets/assets';
 
 const Footer = ({ styles }: { styles: StylesProps }) => {
   console.log('Footer styles:', styles); // styles.footer 값 확인
@@ -7,19 +8,27 @@ const Footer = ({ styles }: { styles: StylesProps }) => {
     return (
       <div id={styles.footer}>
         <div className={styles.contents}>
-          <div className={styles.image}>image</div>
+          <div className={styles.image}>
+            <img src={Assets.innerFactoryIcon} alt='' />
+          </div>
           <span className={styles.title}>내부공장</span>
         </div>
         <div className={styles.contents}>
-          <div className={styles.image}>image</div>
+          <div className={styles.image}>
+            <img src={Assets.outerFactoryIcon} alt='' />
+          </div>
           <span className={styles.title}>외부공장</span>
         </div>
         <div className={styles.contents}>
-          <div className={styles.image}>image</div>
+          <div className={styles.image}>
+            <img src={Assets.escapeGameIcon} alt='' />
+          </div>
           <span className={styles.title}>대탈출</span>
         </div>
         <div className={styles.contents}>
-          <div className={styles.image}>image</div>
+          <div className={styles.image}>
+            <img src={Assets.mypageIcon} alt='' />
+          </div>
           <span className={styles.title}>마이페이지</span>
         </div>
       </div>
