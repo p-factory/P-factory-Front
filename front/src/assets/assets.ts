92a6267dde9f4c6988d8d1cc3cf5f44385a575a1
4Bee <4bee.code@gmail.com>
2025-01-12 16:32:37 +0900

    🐛: 협업으로 덮어진 코드 수정 및 복구

--- front/src/assets/assets.ts
+++ front/src/assets/assets.ts
@@ -1,10 +1,13 @@
 import reactLogo from './img/react.svg';
 import viteLogo from '/vite.svg'; // public 폴더 파일은 루트 경로로 접근 가능
 import escapeGameIcon from './img/escape-game-icon.svg';
+import escapeGameIconHover from './img/escape-game-icon-hover.svg';
 import innerFactoryIcon from './img/inner-factory-icon.svg';
+import innerFactoryIconHover from './img/inner-factory-icon-hover.svg';
 import outerFactoryIcon from './img/outer-factory-icon.svg';
 import outerFactoryIconHover from './img/outer-factory-icon-hover.svg';
 import mypageIcon from './img/mypage-icon.svg';
+import mypageIconHover from './img/mypage-icon-hover.svg';
 import spannerIcon from './img/spanner-icon.svg';
 import cancelIcon from './img/cancel-icon.svg';
 import ptoryMainIcon from './img/ptory-main-logo.svg';
@@ -23,7 +26,9 @@ import highlightIconHover from './img/highlight-icon-hover.svg';
 /* SortButton-Icons */
 import upIcon from './img/up-icon.svg';
 import downIcon from './img/down-icon.svg';
-
+import starIcon from './img/star-icon.svg';
+import starIconChecked from './img/star-icon-checked.svg';
+import moreIcon from './img/more-icon.svg';
 const Assets = {
   reactLogo,
   viteLogo,
@@ -31,8 +36,11 @@ const Assets = {
   innerFactoryIcon,
   outerFactoryIcon,
   mypageIcon,
-  spannerIcon,
+  escapeGameIconHover,
+  innerFactoryIconHover,
   outerFactoryIconHover,
+  mypageIconHover,
+  spannerIcon,
   cancelIcon,
   ptoryMainIcon,
   ptorySubIcon,
@@ -48,6 +56,9 @@ const Assets = {
   highlightIconHover,
   upIcon,
   downIcon,
+  starIcon,
+  starIconChecked,
+  moreIcon,
 };
 
 export default Assets;
