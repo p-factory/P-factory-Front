5ed088eefaf288e21ebe44317547e9dbac737bc7
nyun-nye <138736197+nyun-nye@users.noreply.github.com>
2025-01-20 16:59:25 +0900

    ✨: SignUpCheckList에 버튼 추가

--- shared/components/SignUpCheckList.tsx
+++ shared/components/SignUpCheckList.tsx
@@ -1,5 +1,6 @@
 import { Platform, Text } from 'react-native';
 import { SignUpCheckListStylesLocal } from '../type';
+import { useState, useEffect } from 'react';
 
 const SignUpCheckList = ({
   styles,
@@ -7,33 +8,64 @@ const SignUpCheckList = ({
   styles: SignUpCheckListStylesLocal;
 }) => {
   if (Platform.OS === 'web') {
+    const [isState, setState] = useState<boolean>(false);
+
+    useEffect(() => {
+      setState(false);
+    }, []);
+
     return (
-      <div id={styles.container}>
-        <div className={styles.contents}>
-          <input className={styles.checkbox} type='checkbox' value='option1' />
-          <span className={styles.required}>(필수)</span>
-          <span className={styles.conditions}>서비스 이용약관</span>
+      <form action=''>
+        <div id={styles.container}>
+          <div className={styles.contents}>
+            <input
+              className={styles.checkbox}
+              type='checkbox'
+              value='option1'
+            />
+            <span className={styles.required}>(필수)</span>
+            <span className={styles.conditions}>서비스 이용약관</span>
+          </div>
+          <div className={styles.contents}>
+            <input
+              className={styles.checkbox}
+              type='checkbox'
+              value='option1'
+            />
+            <span className={styles.required}>(필수)</span>
+            <span className={styles.conditions}>개인정보 처리방침</span>
+          </div>
+          <div className={styles.contents}>
+            <input
+              className={styles.checkbox}
+              type='checkbox'
+              value='option1'
+            />
+            <span className={styles.required}>(필수)</span>
+            <span className={styles.conditions}>위치기반서비스 이용약관</span>
+          </div>
+          <div className={styles.contents}>
+            <input
+              className={styles.checkbox}
+              type='checkbox'
+              value='option1'
+            />
+            <span id={styles.selected}>(선택)</span>
+            <span className={styles.conditions}>마켓팅 활용동의</span>
+          </div>
+          <div className={styles.contents}>
+            <input
+              className={styles.checkbox}
+              type='checkbox'
+              value='option1'
+            />
+            <span id={styles.all}>전체동의</span>
+          </div>
         </div>
-        <div className={styles.contents}>
-          <input className={styles.checkbox} type='checkbox' value='option1' />
-          <span className={styles.required}>(필수)</span>
-          <span className={styles.conditions}>개인정보 처리방침</span>
+        <div className={isState ? styles.submit : styles.button}>
+          <button type='submit'>완료</button>
         </div>
-        <div className={styles.contents}>
-          <input className={styles.checkbox} type='checkbox' value='option1' />
-          <span className={styles.required}>(필수)</span>
-          <span className={styles.conditions}>위치기반서비스 이용약관</span>
-        </div>
-        <div className={styles.contents}>
-          <input className={styles.checkbox} type='checkbox' value='option1' />
-          <span id={styles.selected}>(선택)</span>
-          <span className={styles.conditions}>마켓팅 활용동의</span>
-        </div>
-        <div className={styles.contents}>
-          <input className={styles.checkbox} type='checkbox' value='option1' />
-          <span id={styles.all}>전체동의</span>
-        </div>
-      </div>
+      </form>
     );
   }
 
