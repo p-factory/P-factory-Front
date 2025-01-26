ff0c2a0c6f93d7c7533cb2e285557a659dd52ea1
4Bee <4bee.code@gmail.com>
2025-01-26 20:11:26 +0900

    🔥: 불필요한 주석 제거

--- front/src/Model/Hook/useApiMutation.ts
+++ front/src/Model/Hook/useApiMutation.ts
@@ -1,15 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import AxiosInstance from '../axiosInstance';
 
-// remove interface definition
-// interface UseApiQueryAPIProps {
-//   url: string;
-//   nucleus?: any;
-// }
-
-// `onSuccess` or `on~` and `isSuccess` or `is~` syntax obviously different.
-// here generics types definitions default any.
-// Important, first generic represent Parameter type, second generic is returns value type. However, the order of generics is not fixed and can vary depending on the context and use case.
 export const useApiMutation = <InjectNucleus = any, CulturedNucleus = any>(
   method: 'POST' | 'PUT' | 'DELETE',
   url: string = 'api/test',
@@ -18,25 +9,11 @@ export const useApiMutation = <InjectNucleus = any, CulturedNucleus = any>(
   } as InjectNucleus,
 ) => {
   const ApiMutation = async (
-    // The `mutate` function of React Query references these parameters. If called directly outside of `mutate`, this will reference the respective object.
     mutateUrl?: string,
     mutateNucleus?: InjectNucleus,
-    // Remove this type
-    //  : {
-    //   mutation: UseMutationResult<any, Error, { url?: string; nucleus?: T }>;
-    //   isLoading: boolean;
-    //   isError: boolean;
-    //   isSuccess: boolean;
-    //   responseData: any;
-    // }
   ): Promise<CulturedNucleus> => {
     const codeUrl = mutateUrl || url;
     const codeData = mutateNucleus || nucleus;
-
-    // Remove this code
-    // if (!codeUrl) throw new Error('URL is required for the API request.');
-    // if (!method)
-    //   throw new Error('HTTP method is required for the API request.');
     try {
       let dna;
       switch (method) {
@@ -77,22 +54,14 @@ export const useApiMutation = <InjectNucleus = any, CulturedNucleus = any>(
   const mutation = useMutation({
     mutationFn: ApiMutation,
   });
-  //simple use to useMutation
-  /*
-  const mutation = useMutation(fetchMutation);
 
-  return mutation;
-  */
-
-  // The return value definition is used as a status property.
-  // reactQuery ver 5.62.7 doesn't support isLoading.
   const isLoading = mutation.status === 'pending';
   const isError = mutation.status === 'error';
   const isSuccess = mutation.status === 'success';
   const responseData = mutation.data;
-  // Don't confused `useQuery`, useQuery is always search the data from API but, 'useMutation' must be changed to API data
+
   return {
-    mutation, // They includes `mutate, mutateAsync, reset`.
+    mutation,
     isLoading,
     isError,
     isSuccess,
--- front/src/Model/Hook/useApiQuery.ts
+++ front/src/Model/Hook/useApiQuery.ts
@@ -2,21 +2,10 @@
 import { useQuery, useMutation } from '@tanstack/react-query';
 import axiosInstance from '../axiosInstance';
 
-// interface QueryParams {
-//   params?: string; // QueryParams is Optionals
-//   // Another ways use to definition type of the Record
-// }
-
-// Required GET
-// queryKey needs to be defined dynamically. So, Use the functions or Hooks Parameters for this purpose.
-// here is `params` parameter indicate QueryParams
-
 export const useApiQuery = <Organism>(url: string, params?: string) => {
-  // Organism's Generics is not pointed `isLoading, isError, data, isSuccess` state, that arrow the return's data. Don't confused what it is indicated.
-
   const ApiQuery = async () => {
     try {
-      const dna = await axiosInstance.get(url, { params }); // `params` variable type of object support from Axios library So, can't change the name.
+      const dna = await axiosInstance.get(url, { params });
       if (dna.data === true) {
         console.log(`Success: Analysis Complete, check DNA data.`);
       } else {
@@ -31,13 +20,10 @@ export const useApiQuery = <Organism>(url: string, params?: string) => {
   };
 
   const { isLoading, isError, data, isSuccess } = useQuery<Organism>({
-    // queryKey and queryFn is supported from ReactQuery library. That's the Core in the ReactQuery Syntax. please, Remember. the feature of the queryKey and queryFn.
-    queryKey: [url, params], // queryKey must be an array. Its structure depends on the client's requirements.
-
+    queryKey: [url, params],
     queryFn: ApiQuery,
   });
 
-  //`isLoading, isError, data, isSuccess` states are decided what type that are. that's the Big point the ReactQuery
   return {
     isLoading,
     isError,
