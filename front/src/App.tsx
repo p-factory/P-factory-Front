9d67a1d73d31e5c0951180b2cae555af6c495aff
4Bee <4bee.code@gmail.com>
2025-01-30 16:50:31 +0900

    ✨: React-Modal 설치 및 적용

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import TestPage from './View/dev/TestPage';
 import TestFlex from './View/dev/Test.flex';
+import MyFactory from './View/MyFactory';
 // import './App.scss';
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Routes>
       <Route path='/' element={<TestPage />} />
       <Route path='/test' element={<TestFlex />} />
+      <Route path='/MyFactory' element={<MyFactory />} />
     </Routes>
   );
 };
