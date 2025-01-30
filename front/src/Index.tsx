72e6a7c6e53e30367f2ee267d2408f18d6139cc2
4Bee <4bee.code@gmail.com>
2025-01-30 16:51:07 +0900

    ✨: React-Modal 초기값 세팅

--- front/src/Index.tsx
+++ front/src/Index.tsx
@@ -5,6 +5,8 @@ import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Store } from '@shared/store';
+import Modal from 'react-modal';
+Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
