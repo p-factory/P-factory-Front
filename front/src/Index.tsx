a9f6fbe32175e2bb52593f09a45096bdeb21b5d9
4Bee <4bee.code@gmail.com>
2025-01-31 01:00:05 +0900

    ✨: Redux의 slice를 관리하기 위해 디렉토리 생성 및 위치 변경, slice 네이밍 변경 예정, ManagerBar 비활성화시 view보드로 상태 변경 코드 추가 필요

--- front/src/Index.tsx
+++ front/src/Index.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.scss';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { Store } from '@shared/store';
+import { Store } from '@shared/store/store';
 import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
