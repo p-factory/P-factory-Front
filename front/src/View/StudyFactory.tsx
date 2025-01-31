77b431d66b685edb90126a429888adcece21b97f
4Bee <4bee.code@gmail.com>
2025-01-31 23:59:32 +0900

    ✨: 생성된 팩토리 전체적인 HTML 구성 및 간단한 Scss 구현 완료, 디테일한 이미지 및 간격, 폰트크기 미지정

--- front/src/View/StudyFactory.tsx
+++ front/src/View/StudyFactory.tsx
@@ -1,7 +1,27 @@
+import { Sort } from '@shared/components';
+import { SortTypeStyles } from '../Model/Mapping';
+import styled from './StudyFactory.Layout.module.scss';
 const StudyFactory = () => {
   return (
-    <div>
-      <span>StudyFactory</span>
+    <div id={styled.debug}>
+      <div id={styled.header}>
+        <div>
+          <img src='' alt='backIcon' />
+        </div>
+        <span>토익 공부</span>
+        <div>
+          <img src='' alt='starIcon' />
+        </div>
+      </div>
+      <div id={styled.count}>단어 0개</div>
+      <div id={styled.menu}>
+        <div>
+          <Sort styles={SortTypeStyles} />
+        </div>
+        <div>
+          <img src='' alt='downLoadIcon' />
+        </div>
+      </div>
     </div>
   );
 };
