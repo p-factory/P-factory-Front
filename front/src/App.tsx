f6697a9530aa6f878c60a3cec381fa93d7dc7b81
4Bee <4bee.code@gmail.com>
2025-01-31 23:37:34 +0900

    ✨: 생성된 팩토리 단어장 페이지 초기 세팅

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import TestPage from './View/dev/TestPage';
 import TestFlex from './View/dev/Test.flex';
 import MyFactory from './View/MyFactory';
+import StudyFactory from './View/StudyFactory';
 // import './App.scss';
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <Route path='/' element={<TestPage />} />
       <Route path='/test' element={<TestFlex />} />
       <Route path='/MyFactory' element={<MyFactory />} />
+      <Route path='/StudyFactory' element={<StudyFactory />} />
     </Routes>
   );
 };
