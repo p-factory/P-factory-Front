ea522bbbc56ac7c321d548155c9ae87ff54aa3ee
4Bee <109135643+4BFC@users.noreply.github.com>
2025-03-01 00:01:56 +0900

    🔀: API 통신을 점검하고 테스트한다. (#84)
    * ✨: 회원가입 및 로그인 API TEST 통과
    
    - FE에서 구현한 `useApiMutation`은 정상 동작한다, 현재 로그인 시 Refresh-Token은 미구현으로 `useApiQuery` 동작 확인 필요
    
    * ✨: 현재 `responseHeaders`를 useApiMutation에서 작업할 수 있게 구현했다.
    
    - `axiosInstance`에서 `responseHeaders`를 조회 할수 있게 구현 했다. `useApiMutation` return 값을 구현했으나 이를 반환하지 않고 Author가 존재하는 경우 `useApiMutation`에서 Redux나 Cookie에서 관리 할 수 있도록 재구현 필요
    
    * ✨: Api 함수 점검 완료 및 Header를 가져오는 로직을 axiosInstance로 이동
    
    * ✨: useApiQuery, useApiMutation 모두 확인
    
    - useApiQuery 비동기화 점검 필요
    - AxiosInstance 함수에서 Header Author 조회와 자동 추가될 수 있게 구현

--- front/src/Model/axiosInstance.ts
+++ front/src/Model/axiosInstance.ts
@@ -8,4 +8,36 @@ const AxiosInstance = axios.create({
   },
 });
 
+// Axios에서 responseHeaders를 조회할 수 있게 구현
+AxiosInstance.interceptors.response.use(
+  (response) => {
+    console.log('🔥 Response Headers:', response.headers);
+
+    const authorization =
+      response.headers['authorization'] || response.headers['Authorization'];
+    // console.log('🔄 Refresh-Token:', authorization);
+    if (authorization) {
+      localStorage.setItem('access_token', authorization);
+      console.log('🔄Token Accept in localStorage!');
+    }
+    return response;
+  },
+  (error) => {
+    console.error('🚨 Response Error:', error);
+    return Promise.reject(error);
+  },
+);
+
+// Axios에서 모든 요청 author 헤더에 추가
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
 export default AxiosInstance;
