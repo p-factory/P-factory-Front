aa98012c25ffec207ff13f4d29664b7c3f612226
4BFC <4bee.code@gamil.com>
2025-04-01 23:11:29 +0900

    ✨: Modal css 문제 수정 및 적용 확인 완료

--- front/src/Page/Main/Main.tsx
+++ front/src/Page/Main/Main.tsx
@@ -2,36 +2,44 @@ import { ButtonTypeStyles, PtoryLogoTypeStyles } from '@/Model/Mapping';
 import { Button, PtoryLogo } from '@shared/components';
 import { spannerIconWhite } from '@/assets';
 import { useNavigate } from 'react-router-dom';
+import { Main as styles } from '@/View/stylesheet';
+
 const Main = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className={styles.debug}>
       <PtoryLogo styles={PtoryLogoTypeStyles} />
-      <div>
+      <div className={styles.contents}>
         <div>토리와 함께 내가 만들어가는</div>
         <div>
           나만의 <span>단어 공장</span>
         </div>
       </div>
-      <Button
-        styles={ButtonTypeStyles}
-        title='ㅍ토리 사용법'
-        state={null}
-        image={spannerIconWhite}
-        functions={() => navigate('/error')}
-      />
-      <Button
-        styles={ButtonTypeStyles}
-        title='회원가입'
-        state={false}
-        functions={() => navigate('/signUp')}
-      />
-      <Button
-        styles={ButtonTypeStyles}
-        title='단어 공장 작업 시작하기'
-        state={true}
-        functions={() => navigate('/login')}
-      />
+      <div className={styles.button}>
+        <Button
+          styles={ButtonTypeStyles}
+          title='ㅍ토리 사용법'
+          state={null}
+          image={spannerIconWhite}
+          functions={() => navigate('/error')}
+        />
+      </div>
+      <div className={styles.button}>
+        <Button
+          styles={ButtonTypeStyles}
+          title='회원가입'
+          state={false}
+          functions={() => navigate('/signUp')}
+        />
+      </div>
+      <div className={styles.button}>
+        <Button
+          styles={ButtonTypeStyles}
+          title='단어 공장 작업 시작하기'
+          state={true}
+          functions={() => navigate('/login')}
+        />
+      </div>
       {/* <picture>
         <source
           srcSet='https://p-tory-cdn-807801802.imgix.net/img/test.png?fm=webp'
