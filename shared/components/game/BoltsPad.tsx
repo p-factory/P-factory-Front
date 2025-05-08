8d28c0772f78c7c298ad404a0ca8cf6ce56c2de4
구혁모 <gurah129@gmail.com>
2025-05-08 22:27:58 +0900

    ♻️ BoltsPad에서 props로 받은 bolt를 map으로 배치 + 이에 따른 스타일 변경

--- shared/components/game/BoltsPad.tsx
+++ shared/components/game/BoltsPad.tsx
@@ -11,25 +11,21 @@ const BoltsPad = ({
   if (Platform.OS === 'web') {
     {
       const randomElement = useRef(bolt);
-
       /** log를 위한 임시 useEffect 입니다. */
       useEffect(() => {
         const log = (randomElement.current = bolt);
         console.log(log);
-      }, [bolt]);
+      }, []);
 
       return (
         <div className={styles.container}>
           <div className={styles.contents}>
-            {/** 일부 제가 props 값을 설정 했습니다. 이는 단어를 받아서 프론트 내부에서 값을 split 해서 내부에서 단어 배치를 구현하면 되지 않을까 싶어요.  */}
-            <div className={styles.bolt}>b</div>
-            <div className={styles.bolt}>c</div>
-            <div className={styles.bolt}>d</div>
-          </div>
-          <div className={styles.contents}>
-            <div className={styles.bolt}>u</div>
-            <div className={styles.bolt}>p</div>
-            <div className={styles.bolt}>i</div>
+            {/* props로 받은 bolt를 split으로 펼쳐 맵으로 배치  */}
+            {bolt.split('').map((a, index) => (
+              <div className={styles.bolt} key={index}>
+                {a}
+              </div>
+            ))}
           </div>
         </div>
       );
