4f07498d9b769c31cbdac0d17bf29fbbc0ca2507
구혁모 <gurah129@gmail.com>
2025-05-17 03:04:29 +0900

    ✨: 게임 공장 선택 페이지 레이아웃 추가

--- front/src/Page/Game/SettingFactory.tsx
+++ front/src/Page/Game/SettingFactory.tsx
@@ -1,9 +1,119 @@
+import { backIcon } from '@/assets';
+import {
+  ButtonTypeStyles,
+  FactoryTypeStyles,
+  FooterTypeStyles,
+} from '@/Model/Mapping';
 import { SettingFactory as styles } from '@/View/stylesheet';
+import { Button, Factory, Footer } from '@shared/components';
 
 const SettingFactory = () => {
   return (
-    <div className={styles.cotainer}>
-      <div></div>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.img}>
+          <img src={backIcon} alt='back' />
+        </div>
+        <div className={styles.title}>공장 선택</div>
+      </div>
+      <div className={styles.contents}>
+        <span className={styles.description}>
+          대탈출에 나올 공장을 선택해주세요!
+        </span>
+        <div className={styles.factoryContainer}>
+          <Factory
+            id={0}
+            styles={FactoryTypeStyles}
+            outer={true}
+            name={'토익 공부'}
+            favorite={true}
+            total={0}
+            uri={0}
+            handlelocal={() => {
+              console.log('handlelocal');
+            }}
+          />
+          <Factory
+            id={0}
+            styles={FactoryTypeStyles}
+            outer={true}
+            name={'토익 공부'}
+            favorite={false}
+            total={0}
+            uri={0}
+            handlelocal={() => {
+              console.log('handlelocal');
+            }}
+          />
+          <Factory
+            id={0}
+            styles={FactoryTypeStyles}
+            outer={true}
+            name={'토익 공부'}
+            favorite={true}
+            total={0}
+            uri={0}
+            handlelocal={() => {
+              console.log('handlelocal');
+            }}
+          />
+          <Factory
+            id={0}
+            styles={FactoryTypeStyles}
+            outer={true}
+            name={'토익 공부'}
+            favorite={true}
+            total={0}
+            uri={0}
+            handlelocal={() => {
+              console.log('handlelocal');
+            }}
+          />
+          <Factory
+            id={0}
+            styles={FactoryTypeStyles}
+            outer={true}
+            name={'토익 공부'}
+            favorite={true}
+            total={0}
+            uri={0}
+            handlelocal={() => {
+              console.log('handlelocal');
+            }}
+          />
+          <Factory
+            id={0}
+            styles={FactoryTypeStyles}
+            outer={true}
+            name={'토익 공부'}
+            favorite={true}
+            total={0}
+            uri={0}
+            handlelocal={() => {
+              console.log('handlelocal');
+            }}
+          />
+        </div>
+      </div>
+      <div className={styles.choiceContainer}>
+        <Button
+          styles={ButtonTypeStyles}
+          state={true}
+          title='전체 선택'
+          functions={() => {
+            console.log('all choice');
+          }}
+        />
+        <Button
+          styles={ButtonTypeStyles}
+          state={true}
+          title='완료'
+          functions={() => {
+            console.log('complate');
+          }}
+        />
+      </div>
+      <Footer styles={FooterTypeStyles} />
     </div>
   );
 };
